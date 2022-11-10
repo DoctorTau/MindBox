783dca1bfa414a207fa9ae14a09335cb9f516ece
DrTau <drttau@gmail.com>
2022-11-10 10:07:48 +0300

    Added right triangle checker.

--- Triangle.cs
+++ Triangle.cs
@@ -19,4 +19,15 @@ public class Triangle : Figure
         * (sideA - sideB + sideC)
         * (-sideA + sideB + sideC)) / 4;
     }
+
+    public bool IsRightTriangle()
+    {
+        double squareA = Math.Pow(sideA, 2),
+               squareB = Math.Pow(sideB, 2),
+               squareC = Math.Pow(sideC, 2);
+
+        return squareA == squareB + squareC
+            || squareB == squareA + squareC
+            || squareC == squareA + squareB;
+    }
 }
